e285cd8075fb52eef8967f158ecd938a647cc6ef
SanMarABr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-05 14:09:56 -0600

    Primer ejemplo if

--- src/main/java/SentenciasControl/SentenciaIf.java
+++ src/main/java/SentenciasControl/SentenciaIf.java
@@ -2,6 +2,13 @@ package SentenciasControl;
 
 public class SentenciaIf {
     public static void main(String[] args) {
-        System.out.println("Sentencia if");
+        var condicion = true;
+
+        if (condicion == true) {
+            System.out.println("Es verdadera");
+            System.out.println("Nueva linea");
+        } else {
+            System.out.println("Condicion Falsa");
+        }
     }
 }
