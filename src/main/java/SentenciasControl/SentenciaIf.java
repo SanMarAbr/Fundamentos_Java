d54b929da8f357cb54892903c0bb1c0b81a63163
SanMarABr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-06 00:40:26 -0600

    Se añaede programa SentenciaIF

--- src/main/java/SentenciasControl/SentenciaIf.java
+++ src/main/java/SentenciasControl/SentenciaIf.java
@@ -10,5 +10,18 @@ public class SentenciaIf {
         } else {
             System.out.println("Condicion Falsa");
         }
+
+        var numero = .0 ;
+        var numeroDesconocido = "no es valido";
+
+        if (numero == 1) {
+            System.out.println("Numero 1");
+        } else if(numero ==2 ){
+            System.out.println("Numero 2");
+        } else if(numero == 3){
+            System.out.println("Numero 3");
+        } else {
+            System.out.println(numeroDesconocido);
+        }
     }
 }
