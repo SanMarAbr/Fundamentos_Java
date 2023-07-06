09c2c70b664ccd3bc72843be0a2d8a514fde9e18
SanMarABr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-06 10:34:27 -0600

    Agrega programa estaciones de año con String

---
+++ src/main/java/SentenciasControl/EstacionesAño.java
@@ -0,0 +1,41 @@
+//Utilizar sentencia if para mostrar las estaciones del año
+//Por medio de operador logico OR
+
+package SentenciasControl;
+
+import java.util.Scanner;
+
+public class EstacionesAño {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String estacionAño = "";
+        String mesDesconocido = "Mes desconocido";
+        System.out.println("Estaciones del año");
+        System.out.println("Ingrese el mes");
+        String mes = scanner.nextLine();
+
+        if (mes.equals("Enero") || mes.equals ("Febrero") || mes.equals("Marzo") || mes.equals("Diciembre")) {
+
+            estacionAño = "Invierno";
+
+        } else if (mes.equals("Abril") || mes.equals ("Mayo ") || mes.equals("Junio")) {
+
+            estacionAño = "Primavera";
+
+        } else if (mes.equals("Julio") || mes.equals("Agosto")  || mes.equals("Septiembre")) {
+
+            estacionAño = "Verano";
+
+        } else if (mes.equals("Octubre") || mes.equals("Noviembre")){
+
+            estacionAño = "Otoño";
+
+        } else {
+
+            System.out.println(mesDesconocido);
+        }
+            System.out.println(estacionAño);
+
+        scanner.close();
+    }
+}
