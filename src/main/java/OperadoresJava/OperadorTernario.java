708002ec28e5eb83ec445ef322b85357b5f88a7a
SanMarABr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-06 12:47:39 -0600

    Comentar este programa

--- src/main/java/OperadoresJava/OperadorTernario.java
+++ src/main/java/OperadoresJava/OperadorTernario.java
@@ -4,12 +4,12 @@ public class OperadorTernario {
 
     public static void main(String[] args) {
         //Expresion a evaluar ||| Caso verdadero ||| Falso 
-        var resultado = (3 > 2) ? "Verdadero" : false; //Solo funciona con valores iguales
-        System.out.println("Resultado = " + resultado);
+        // var resultado = (3 > 2) ? "Verdadero" : false; //Solo funciona con valores iguales
+        // System.out.println("Resultado = " + resultado);
 
-        var numero = 7;
-        var par = (numero % 2 == 0) ? "numero par" : "numero impar";
-        System.out.println("par = "+ par);
+        // var numero = 7;
+        // var par = (numero % 2 == 0) ? "numero par" : "numero impar";
+        // System.out.println("par = "+ par);
 
     }
 }
