9f11711399a8e37290aa8cb848e58d99bc30e920
SanMarABr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-09 13:04:01 -0600

    Agregan 2 programas sencillos de ciclos

---
+++ src/main/java/Ciclos/CicloWhile.java
@@ -0,0 +1,14 @@
+package Ciclos;
+
+public class CicloWhile {
+    public static void main(String[] args) {
+
+        var contador = 0;
+        
+        while (contador < 3) {
+            System.out.println("Contador = " + contador);
+            contador++;
+        }
+
+    }
+}
