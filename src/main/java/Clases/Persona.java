5fd9025d405eb3d9405267c1dd4c194545c56f67
SanMarABr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-09 14:38:40 -0600

    Programas Clases con carpeta

---
+++ src/main/java/Clases/Persona.java
@@ -0,0 +1,25 @@
+package Clases; //Especifica el nombre del paquete almacenado en esta clase
+
+public class Persona 
+{
+    //Atributos
+        //nombre : String
+        //genero : char
+        //ocupacion : String
+    //Metodos
+        //obtenerNombre() : String
+        //obtenerGenero() : String
+        //modificarNombre (nombre: String)
+        //modificarGenero (genero : String)
+        //modificarOcupacion (ocupacion : String)
+    //Atributos
+    String nombre;
+    String apellido;
+
+    public void desplegarInformacion()
+    {
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
+
+}
---
+++ src/main/java/Clases/PruebaPersona.java
@@ -0,0 +1,9 @@
+package Clases;
+
+public class PruebaPersona {
+    public static void main(String[] args) {
+        Persona persona1;
+        persona1 = new Persona();
+        
+    }
+}
