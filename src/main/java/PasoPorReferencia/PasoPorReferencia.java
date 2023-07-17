0873e9b105b21e789133098cb9194bca65bb2027
SanMarAbr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-17 11:33:01 -0600

    Return

--- src/main/java/PasoPorReferencia/PasoPorReferencia.java
+++ src/main/java/PasoPorReferencia/PasoPorReferencia.java
@@ -9,13 +9,14 @@ public class PasoPorReferencia{
         Persona persona1 = new Persona(); //Hereda de clases Object
         persona1.nombre = " Abraham";
         System.out.println("Persona1 nombre: "+persona1.nombre);
-        cambiaValor(persona1);
+        persona1 = cambiaValor(persona1);
         System.out.println("Persona1 cambio nombre: "+ persona1.nombre);
     }
-    public static void cambiaValor(Persona persona){
+    
+    public static Persona cambiaValor(Persona persona){
       //  System.out.println("Persona1: "+persona.nombre);
         persona.nombre = "Laura";
-
+        return persona;
     }
 
     
