1f4daa070b898213e7f60e0fb1fcfa4c0bf88bd2
SanMarAbr <64150546+SanMarAbr@users.noreply.github.com>
2023-07-17 11:11:40 -0600

    Paso por valor, se pasa una copia de la variable, el metodo no modificara

--- src/main/java/PasosPorValor/PasoPorValor.java
+++ src/main/java/PasosPorValor/PasoPorValor.java
@@ -1,6 +1,19 @@
 package PasosPorValor;
 
 public class PasoPorValor {
-    
-    
+    public static void main(String[] args) {
+
+        int x = 10;
+        System.out.println("X: " + x);
+
+        cambiarValor(x);
+        System.out.println("X nuevo valor: "+x);
+    }
+
+    public static void cambiarValor(int arg1) {
+        System.out.println("Iniciando Metodo");
+        System.out.println("Variable arg1: " + arg1);
+        arg1 = 15;
+    }
+
 }
